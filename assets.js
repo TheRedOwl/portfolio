d286e7f3e839f865948718c0d6538681e304aed5
Your Name <you@example.com>
2024-01-31 13:31:23 +0100

    about page error

---
+++ assets.js
@@ -0,0 +1,11 @@
+export const motto="dream big and don't wake up".split(" ")
+
+export const panelBackgrounds = [
+    'https://source.unsplash.com/black-and-orange-sports-coupe-aIDkOU7eGgo',
+    'https://source.unsplash.com/white-and-black-chevrolet-camaro-on-road-during-daytime-TbCNH5D3W0c',
+    'https://images.unsplash.com/photo-1465188162913-8fb5709d6d57',
+    'https://source.unsplash.com/ITjiVXcwVng/1500x1500&#39',
+    'https://source.unsplash.com/3MNzGlQM7qs/1500x1500&#39',
+    'https://source.unsplash.com/tuEtpjghVmg/1500x1500&#39'
+
+];
