77052c001c89a12720d630f73a6ec10ca5a3a356
Your Name <you@example.com>
2024-02-07 14:00:36 +0100

    json

--- projects.js
+++ projects.js
@@ -8,10 +8,19 @@ export const projects = () => {
         console.log(data);
         data.forEach((element) => {
             document.querySelector(".projectsData").innerHTML += `
-                <div class="button col-md-6">
-                    <h1>${element.name}</h1>
-                    <span>${element.desciption}</span>
-                </div>`;
+            <div class="card" id="project${element.id}" style="width: 18rem;">
+            <img data-src="${element.img}" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">${element.name}</h5>
+                <p class="card-text">${element.description}</p>
+            </div>
+            <div class="card-body projectButtons">
+                <a href="${element.hosted}" target="blank_" class="btn btn-primary">Go to the page!</a>
+                <a href="${element.github}" target="blank_" class="btn btn-primary"><i class="fa-brands fa-github fa-xl"></i></a>
+                <a href="${element.imgs}" target="blank_" class="btn btn-primary"><i class="fa-solid fa-image fa-xl"></i></a>
+            </div>
+        </div>
+            `;
         });
     }
 };
