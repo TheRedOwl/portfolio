d6e2c880095a580dc130d20c292a015d738ae9ac
TheRedOwl <v.adam3000@gmail.com>
2024-02-14 06:37:27 +0100

    about last things

--- about.js
+++ about.js
@@ -1,7 +1,14 @@
 import { motto, panelBackgrounds } from "./assets.js";
+import { getData } from "./assets.js";
+
+
 
 export const about = () => {
-    motto.forEach((word)=>(document.querySelector(".panels").innerHTML += `<div class="panel">${word}</div>`));
+
+    motto.forEach((word,idNum)=>{
+        document.querySelector(".panels").innerHTML += `<div class="panel id${idNum+1}">${word}</div>`;
+        idNum+=1
+    });
 
     let panelsNList = document.querySelectorAll(".panel");
     panelsNList.forEach(
@@ -10,16 +17,42 @@ export const about = () => {
     );
 
     document.querySelector(".panels").addEventListener("click", toggleOpen);
+    const url = "https://raw.githubusercontent.com/TheRedOwl/portfolio/master/aboutInfo.json";
+    
+    getData(url, aboutText);
+
+    function aboutText(aboutData){
+        aboutData.forEach((elements) => {
+            document.querySelector(".aboutText").innerHTML += `
+            <div class="d-flex">
+                <div class="text">
+                    <h2>${elements.topic}</h2>
+                    <p>${elements.text}</p>
+                </div>
+                <div class="pic">
+                    <img src="${elements.picture}" class="img-fluid" alt="">
+                </div>
+            </div>`
+        })
+    }
 
     function toggleOpen(e) {
+        const numbers=[1,2,3,4,5,6]
         if (e.target.classList.contains("open")) {
             e.target.classList.remove("open");
         } else {
             panelsNList.forEach((obj) => obj.classList.remove("open"));
             e.target.classList.add("open");
-            console.log(e.target);
+            numbers.forEach((num)=>{
+                if(e.target.classList.contains(`id${num}`)){
+
+                }
+            })
+
         }
     }
+
+    
 
     
 };
